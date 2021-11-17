810451af92540443203b154321b7e645130cb970
Pavel Biychuk <paul.biychuk@yandex.by>
2021-11-17 19:53:39 +0300

    urls for flake8

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views
-
-app_name = 'infra_app'
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
-    path('second_page/', views.second_page, name='second_page'),
 ]
